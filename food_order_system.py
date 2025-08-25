81070ac968165d8a4b73941daa7a75126611c463
nsard870 <sardinicolas@gmail.com>
2025-08-25 15:44:42 +0200

    Update food_order_system.py

--- food_order_system.py
+++ food_order_system.py
@@ -5,28 +5,49 @@ italian_food = [
   "Lasagna"
 ]
 
+indian_food = [
+  "Curry",
+  "Chutney",
+  "Samosa",
+  "Naan"
+]
+
 def find_meal(name, menu):
   return name if name in menu else None
 
-def select_meal(name):
-  return find_meal(name, italian_food)
+def select_meal(name, food_type):
+  if food_type == "Italian" :
+    return find_meal(name, italian_food)
+  elif food_type == "Indian" :
+    return find_meal(name, indian_food)
+  else :
+    return None
 
-def display_available_meals():
-  print("Available Italian Meals:")
-  for meal in italian_food:
-    print(meal)
+def display_available_meals(food_type):
+  if food_type == "Italian" :
+    print("Available Italian Meals:")
+    for meal in italian_food:
+      print(meal)
+  elif food_type == "Indian" :
+    print("Available Indian Meals:")
+    for meal in indian_food:
+      print(meal)
+  else :
+    print("Invalid food type")
 
-def create_summary(name, amount):
-  order = select_meal(name)
+
+def create_summary(name, amount, food_type):
+  order = select_meal(name, food_type)
   if order:
     return f"You ordered {amount} {name}"
   else:
     return "Meal not found"
 
 print("Welcome to the Food Order System!")
-display_available_meals()
+type_input = input("Choose your type of food: ")
+display_available_meals(type_input)
 name_input = input("Enter the name of the meal you want to order: ")
 amount_input = int(input("Enter the quantity you want to order: "))
 
-result = create_summary(name_input, amount_input)
+result = create_summary(name_input, amount_input, type_input)
 print(result)
